500cb4f2c518b42d1fb1bf287c9dc108b3bf7eae
Jose Garcia <jose@Joses-MacBook-Air.local>
2023-04-25 14:51:39 -0400

    added routes

---
+++ routes/api/category-routes.js
@@ -0,0 +1,76 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  Category.findAll({
+		include: [Product],
+	}).then((results) => {
+		res.json(results);
+	});
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+			id: req.params.id,
+		},
+		include: [Product],
+	}).then((results) => {
+		if (!results) {
+			res.status(404).json({ messsage: "No category found with this id" });
+			return;
+		}
+		res.json(results);
+	});
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+  console.log(req.body)
+  Category.create({
+	id: req.body.id,
+	category_name: req.body.category_name,
+}).then((results) => {
+	res.json(results);
+});
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+  Category.update(
+	{
+		category_name: req.body.category_name,
+	},
+	{	
+		where: {
+			id: req.params.id,
+		},
+	}	
+	).then((results) => {
+		res.json(results);
+	});
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  Category.destroy({
+		where: {
+			id: req.params.id,
+		},
+	})
+	.then((results) => {
+		if (!results) {
+			res.status(404).json({ messsage: "No category found with this id" });
+			return;
+		}
+		res.json(results);
+	});
+});
+
+module.exports = router;
